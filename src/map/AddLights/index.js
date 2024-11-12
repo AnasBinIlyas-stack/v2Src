39a65f5075aa414b2a4c6cf9c4efb5292e39c13a
AnasBinIlyas-stack <146701088+AnasBinIlyas-stack@users.noreply.github.com>
2024-11-11 21:13:42 -0800

    map fix

--- src/map/AddLights/index.js
+++
@@ -1,9 +0,0 @@
-import * as THREE from 'three'
-
-function addLights(scene) {
-    var ambientLight = new THREE.AmbientLight(0xffffff, 3)
-
-    scene.add(ambientLight)
-}
-
-export default addLights
